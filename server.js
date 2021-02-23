12393c79e9456369d4005628057480e01d6489e1
Isidro Lopez <isidrolopez@Isidros-MacBook-Pro.local>
2021-02-23 14:43:52 -0500

    Latest version

--- server.js
+++ server.js
@@ -1,19 +1,64 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+const data = [];
 // Require Express to run server and routes
+const express = require('express');
+
 
 // Start up an instance of app
+const app = express();
+
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+	function listening() {
+	    console.log('Running on port '+port);
+	}
+
+
+
+// GET route that retuns projectData
+app.get('/all', sendData);
+
+function sendData (request, response) {
+  response.send(data);
+};
+
+
+// POST route
+app.post('/add', callBack);
+
+function callBack(req,res){
+  res.send('POST received');
+};
+
+app.post('/addData', addData);
+
+function addData (req,res){
+	newEntry = {
+		temperature: req.body.temperature,
+		date: req.body.date,
+		userResponse: req.body.userResponse
+	}
+
+    data.push(newEntry);
+    res.send(data);
+    console.log("My project data: ");
+    console.log(data);
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,70 @@
 /* Global Variables */
+let myOpenWeatherMapAPIKey = "ea3ab8e55606cf6bd6983f2eaf2387aa";
+let cityName = "33155";
+let api_baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Async POST
+const postData = async ( url = '', data = {})=>{
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  	});
+    try {
+      const newData = await response.json();
+      console.log("mynewdata");
+      console.log(newData);
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+
+//Async - GET
+const retrieveData = async (base_url=api_baseURL, zipCode=document.getElementById("zip").value, personalAPIKey = myOpenWeatherMapAPIKey) =>{ 
+  let url = base_url+zipCode+"&appid="+personalAPIKey;
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  console.log(allData);
+  return allData;
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+//EventListener for the element with ID generate 
+document.getElementById("generate").addEventListener("click", functionToRunOnClickGenerate);
+
+function functionToRunOnClickGenerate() {
+	retrieveData().then(function(allData){
+	postData('/addData', {temperature: allData.main.temp, date: newDate, userResponse: document.getElementById("feelings").value})
+	updateUI();
+});
+}
+
+const updateUI = async () => {
+	const request = await fetch('/all')
+	try {
+		const allData = await request.json();
+		console.log(allData);
+		var highest = allData[ Object.keys(allData).sort().pop() ];
+		document.getElementById("date").innerHTML = highest.date;
+		document.getElementById("temp").innerHTML = highest.temperature;
+		document.getElementById("content").innerHTML = highest.userResponse;
+
+	} catch(error) {
+		console.log("error", error);
+	}
+}
